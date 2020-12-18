e12cbb8fd2bd90bc747e783be28477747073c3ee
Peace <pk568@cornell.edu>
2020-12-18 15:51:34 -0500

    fix bugs

--- MFVNeutralino/plugins/Vertexer.cc
+++ MFVNeutralino/plugins/Vertexer.cc
@@ -841,7 +841,7 @@ void MFVVertexer::produce(edm::Event& event, const edm::EventSetup& setup) {
       bool merge = false;
       for (v[1] = v[0] + 1; v[1] != vertices->end(); ++v[1]) {
 		std::cout << __LINE__ << std::endl;
-		std::cout << vertices->end() << std::endl;
+		std::cout << vertices.size() << std::endl;
         ivtx[1] = v[1] - vertices->begin();
 		
 		std::cout << __LINE__ << std::endl;
@@ -877,7 +877,7 @@ void MFVVertexer::produce(edm::Event& event, const edm::EventSetup& setup) {
 			  for (int i = 0; i < 2; ++i) {
 				  std::cout << __LINE__ << std::endl;
 				  std::cout << i << ": " << vertex_track_set(*v[i]).size() << std::endl;
-				  std::cout << v[i] - vertices->begin() << "in" << vertices->end() << std::endl;
+				  std::cout << v[i] - vertices->begin() << "in" << vertices.size() << std::endl;
 				  std::cout << __LINE__ << std::endl;
 				  for (auto tk : vertex_track_set(*v[i])) {
 					  
@@ -939,9 +939,9 @@ void MFVVertexer::produce(edm::Event& event, const edm::EventSetup& setup) {
 				  *v[0] = new_vertices[0];
 				  std::cout << "check no mem out of ranges (after) : " << v[1] - vertices->begin() << std::endl;
 				  std::cout << __LINE__ << std::endl;
-				  std::cout << vertices->end() << std::endl;
+				  std::cout << vertices.size() << std::endl;
 				  vertices->erase(v[1]);
-				  std::cout << vertices->end() << std::endl;
+				  std::cout << vertices.size() << std::endl;
 				  std::cout << __LINE__ << std::endl;
 			  }
 			  // pk: change vertices
