30b86e6f4894cb9fd0078534739aba78a978a263
Peace <pk568@cornell.edu>
2021-01-27 11:50:35 -0500

    relax fiducial cuts

--- Tools/scripts/null_SV_default_ntuple.py
+++ Tools/scripts/null_SV_default_ntuple.py
@@ -82,7 +82,7 @@ for event1 in events_ntuple1 :
 
     nevents_processed += 1
     if nevents_processed <= 5000 :
-         if 0.0150 < math.sqrt((mevent.gen_lsp_decay[0])**2 + (mevent.gen_lsp_decay[1])**2) < 2 and  0.0150 < math.sqrt((mevent.gen_lsp_decay[3])**2 + (mevent.gen_lsp_decay[4])**2) < 2 and math.fabs(ROOT.reco.deltaPhi(mevent.gen_lsp_phi[0], mevent.gen_lsp_phi[1])) > 2.7: # apply fiducial cuts
+         if 0.0100 < math.sqrt((mevent.gen_lsp_decay[0])**2 + (mevent.gen_lsp_decay[1])**2) < 2 and  0.0100 < math.sqrt((mevent.gen_lsp_decay[3])**2 + (mevent.gen_lsp_decay[4])**2) < 2 and math.fabs(ROOT.reco.deltaPhi(mevent.gen_lsp_phi[0], mevent.gen_lsp_phi[1])) > 2.7: # apply fiducial cuts
         #if math.fabs(ROOT.reco.deltaPhi(mevent.gen_lsp_phi[0], mevent.gen_lsp_phi[1])) > 2.7: # no apply fiducial cuts
             
                nevents_fiducial_cuts += 1
