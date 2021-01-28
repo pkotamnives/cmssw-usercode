9c816ee18822d8e132cbe0705432e25c6d20e609
Peace <pk568@cornell.edu>
2021-01-28 09:47:53 -0500

    eff plot

--- Tools/scripts/results_err_plots.py
+++ Tools/scripts/results_err_plots.py
@@ -29,10 +29,10 @@ outfile = ROOT.TFile(outputdir+"out.root", "RECREATE")
 # Define histograms here (obviously this one doesn't matter for you, but I stole it from some other code of mine)
 
 n = 4
-x = [1.0,2.0,3.0,4.0]
-y1 = [41.1,66.9,73.8,80.6]
+x = [3.0,5.0,7.0,9.0]
+y1 = [90.88,89.85,88.87,93.53]
 ex = [0.0,0.0,0.0,0.0]
-ey1 = [1.3,0.9,1.0,0.5]
+ey1 = [0.26,0.27,0.28,0.22]
 
 x = np.array(x)
 y1 = np.array(y1)
@@ -40,11 +40,12 @@ ex = np.array(ex)
 ey1 = np.array(ey1)
 
 
-y2 = [41.1,85.6,90.2,95.9]
-ey2 = [1.3,0.7,0.7,0.2]
+y2 = [90.9,90.81,89.41,93.42]
+ey2 = [0.23,0.23,0.24,0.20]
 y2 = np.array(y2)
 ey2 = np.array(ey2)
 
+"""
 y3 = [44.7,65.1,71.4,79.4]
 ey3 = [1.3,0.9,1.0,0.5]
 y3 = np.array(y3)
@@ -54,19 +55,21 @@ y4 = [31.5,76.8,83.6,93.2]
 ey4 = [1.2,0.8,0.8,0.3]
 y4 = np.array(y4)
 ey4 = np.array(ey4)
+"""
 
 mg = ROOT.TMultiGraph()
 
 g1 = ROOT.TGraphErrors(4,x,y1,ex,ey1)
 g1.SetLineColor( ROOT.kBlue )
-g1.SetTitle( "average shared-track pT" )
+g1.SetTitle( "multijets" )
 g1.SetLineWidth( 2 )
 #g1.SetMarkerStyle(21)
 g2 = ROOT.TGraphErrors(4,x,y2,ex,ey2)
 g2.SetLineColor( ROOT.kRed )
-g2.SetTitle( "sum shared-track pT" )
+g2.SetTitle( "dijets" )
 g2.SetLineWidth( 2 )
 #g2.SetMarkerStyle(21)
+"""
 g3 = ROOT.TGraphErrors(4,x,y3,ex,ey3)
 g3.SetLineColor( ROOT.kOrange )
 g3.SetTitle( "ratio average shared-track pT" )
@@ -76,14 +79,15 @@ g4 = ROOT.TGraphErrors(4,x,y4,ex,ey4)
 g4.SetLineColor( ROOT.kGreen )
 g4.SetTitle( "ratio sum shared-track pT" )
 g4.SetLineWidth( 2 )
+"""
 #g4.SetMarkerStyle(21)
 mg.Add(g1)
 mg.Add(g2)
-mg.Add(g3)
-mg.Add(g4)
+#mg.Add(g3)
+#mg.Add(g4)
 
 
-mg.SetTitle("Multijets: Validation of pT variables by varying shared-track ratios; # major vtx's shared tracks/ # of minor vtx's shared tracks; signal efficiency (%)")
+mg.SetTitle("Signal efficiency after removing shared tracks and apply >=5trks/vtx; Method #; signal efficiency by pT variables (%)")
 
 
 # make a canvas, draw, and save it
@@ -92,6 +96,6 @@ mg.SetTitle("Multijets: Validation of pT variables by varying shared-track ratio
 c0 = ROOT.TCanvas()
 mg.Draw("ALP")
 c0.BuildLegend(0.45,0.21,0.75,0.51)
-c0.Print (outputdir+"plot_tbs.png")
-c0.Print (outputdir+"plot_tbs.root")
+c0.Print (outputdir+"plot_eff.png")
+c0.Print (outputdir+"plot_eff.root")
 
